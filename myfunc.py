456061ff8a8cea4ce7e845c1661d834f337db383
Zhao Qiang <qiang.zhao@nxp.com>
2018-10-30 14:59:16 +0800

    add function for tar images as solution
    Signed-off-by: Zhao Qiang <qiang.zhao@nxp.com>

--- myfunc.py
+++ myfunc.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 
+import os
+import shutil
+
 filename = "./config/build_lsdk_internal.cfg"
+imagedir = "/home/zhaoqiang/work/flexbuild/build/images/"
+soluname = "edgescale-solution.tgz"
+firmware = "firmware_ls1043ardb_uboot_sdboot.img"
+bootpartition = "bootpartition_arm64_lts_4.9.tgz"
+rootfs = "rootfs_ubuntu_bionic_arm64.tgz"
+downdir = "/home/zhaoqiang/work/python/csdk-draft/download/"
 
 def enEdge():
     file_data = ""
@@ -12,5 +21,15 @@ def enEdge():
     with open(filename, "w") as f:
         f.write(file_data)
 
+def mksolution():
+    os.chdir(imagedir)
+    commandtar = "tar -czvf " + soluname + " " + os.readlink(bootpartition) + \
+            " " + firmware + " " + os.readlink(rootfs)
+    os.system(commandtar)
+    destpath = downdir + soluname
+    if os.path.exists(destpath):
+        os.remove(destpath)
+    shutil.move(soluname, downdir)
+
 if __name__ == '__main__':
-    enEdge()
+    mksolution()
